4e2cfd87d12aadd9d02d7a8d6fff3cd02949b77d
ElvinSadikhov <elvin.279.6s@gmail.com>
2022-04-21 15:31:31 +0400

    changed code

--- lib/isolates_and_async.dart
+++ lib/isolates_and_async.dart
@@ -9,34 +9,46 @@ import 'package:encrypt/encrypt.dart';
 class EncryptData {
 //for AES Algorithms
 
-  static Encrypted? encrypted;
-  static var decrypted;
+  static Encrypted? _encrypted;
+  static var _decrypted;
 
-  static encryptAES(List<dynamic> data) async {
+  static Future<Encrypted> encryptInBackground(String message) async {
+    final receivePort = ReceivePort();
+    Isolate.spawn(EncryptData._encryptAES, [receivePort.sendPort, message]);
+    return await receivePort.first;
+  }
+
+  static Future<String> decryptInBackground(Encrypted encrypted) async {
+    final receivePort = ReceivePort();
+    Isolate.spawn(EncryptData._decryptAES, [receivePort.sendPort, encrypted]);
+    return await receivePort.first;
+  }
+
+  static _encryptAES(List<dynamic> data) async {
     SendPort sendPort = data[0];
     String plainText = data[1];
 
     final key = Key.fromUtf8('my 32 length key................');
     final iv = IV.fromLength(16);
     final encrypter = Encrypter(AES(key));
-    encrypted = encrypter.encrypt(plainText, iv: iv);
+    _encrypted = encrypter.encrypt(plainText, iv: iv);
 
-    Isolate.exit(sendPort, encrypted);
+    Isolate.exit(sendPort, _encrypted);
   }
 
-  static decryptAES(List<dynamic> data) {
+  static _decryptAES(List<dynamic> data) {
     SendPort sendPort = data[0];
     Encrypted? encrypted = data[1];
 
     final key = Key.fromUtf8('my 32 length key................');
     final iv = IV.fromLength(16);
     final encrypter = Encrypter(AES(key));
-    decrypted = encrypter.decrypt(encrypted!, iv: iv);
+    _decrypted = encrypter.decrypt(encrypted!, iv: iv);
 
     Future.delayed(
         Duration(seconds: 5),
         () => {
-              Isolate.exit(sendPort, decrypted),
+              Isolate.exit(sendPort, _decrypted),
             });
 
     // OR without delay:
--- lib/main.dart
+++ lib/main.dart
@@ -7,36 +7,22 @@ import 'isolates_and_async.dart';
 import 'dart:isolate';
 
 void main() async {
-  /*
-  // 1st way: (actually it is not the way, just we await for the first call)
-  // Encrypted encrypted = await encryptInBackground("wha's up");
-  // decryptInBackground(encrypted).then((decrypted) => {print(decrypted)});
+  print("Code is executing");
 
-  // 2nd way:
-  encryptInBackground("hii").then((encrypted) => {
-        decryptInBackground(encrypted).then((decrypted) => {
-              print(decrypted),
-            })
+  EncryptData.encryptInBackground("hii").then((encrypted) => {
+        EncryptData.decryptInBackground(encrypted)
+            .then((decrypted) => {print(decrypted)})
       });
 
+  print("Start counting");
   // some routine, to show that the isolate is working
   for (int i = 1; i <= 10; i++) {
     await Future.delayed(Duration(seconds: 1), () => {print(i)});
   }
-  */
+}
+
+void filing() {
   File myFile = File('C:\\Users\\Elvin\\Desktop\\myFile.txt');
   myFile.writeAsStringSync("hello,");
   myFile.writeAsStringSync("hi man", mode: FileMode.append);
-}
-
-Future<Encrypted> encryptInBackground(String message) async {
-  final receivePort = ReceivePort();
-  Isolate.spawn(EncryptData.encryptAES, [receivePort.sendPort, message]);
-  return await receivePort.first;
-}
-
-Future<String> decryptInBackground(Encrypted encrypted) async {
-  final receivePort = ReceivePort();
-  Isolate.spawn(EncryptData.decryptAES, [receivePort.sendPort, encrypted]);
-  return await receivePort.first;
 }
